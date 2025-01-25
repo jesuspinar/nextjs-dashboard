1f35b34a1c9de7972ae6cbc8d0174464fb2e47ec
Jesus Pinar <devjesuspinar@gmail.com>
2025-01-25 12:47:20 +0000

    Add error messages inputs at create form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,25 +5,47 @@ import connectionPool from '../../db';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   try {
-    const { customerId, amount, status } = CreateInvoice.parse({
+    const validatedFields = CreateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing or Invalid Fields. Failed to Create Invoice.'
+      };
+    }
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -35,8 +57,10 @@ export async function createInvoice(formData: FormData) {
 
     await connectionPool.query(query);
 
-  } catch (error: any) {
-    console.error('Database Error:', error.message, error.stack);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Database Error:', error.message, error.stack);
+    }
   }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices')
@@ -59,8 +83,10 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     await connectionPool.query(query);
 
-  } catch (error: any) {
-    console.error('Error updating invoice:', error.message, error.stack);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error updating invoice:', error.message, error.stack);
+    }
     throw new Error('An error occurred while updating the invoice.');
   }
   revalidatePath('/dashboard/invoices');
@@ -76,8 +102,10 @@ export async function deleteInvoice(id: string) {
 
     await connectionPool.query(query);
 
-  } catch (error: any) {
-    console.error('Error deleting invoice:', error.message, error.stack);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Error deleting invoice:', error.message, error.stack);
+    }
     throw new Error('An error occurred while deleting the invoice.');
   }
   revalidatePath('/dashboard/invoices');
