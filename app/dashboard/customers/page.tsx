8b2cd49197d995c147ed6fa75d6dab47e1add4ba
Jesus Pinar <devjesuspinar@gmail.com>
2025-01-27 10:58:08 +0000

    Add table to customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,28 @@
+import Table from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from "next";
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customer Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
